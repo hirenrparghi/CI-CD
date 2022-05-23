7eaded99f6ca6c43ce906933d8908c233ec38dd6
Hiren Parghi <hirenparghi90@gmail.com>
2022-05-23 12:38:22 +0530

    12:38

--- test.ps1
+++ test.ps1
@@ -1,7 +1,5 @@
 cd C:\inetpub\wwwroot\
 git clone https://github.com/hirenrparghi/CI-CD.git
-rm -R C:\inetpub\wwwroot\CI-CD\.github
-cd C:\inetpub\wwwroot\CI-CD\
-ls
-cd C:\inetpub
+rm -R C:\inetpub\wwwroot\CI-CD\.github\
+cd C:\inetpub\wwwroot\CI-CD\SampleWebApplication
 ls
